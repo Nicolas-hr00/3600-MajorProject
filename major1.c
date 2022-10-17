e8795e69196310cc3da4b1329763dc8a366ad047
Tsuchiya <ert0068@cell01-cse.unt.ad.unt.edu>
2022-10-16 19:35:18 -0500

    added major1.c

---
+++ major1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+//using namespace std;
+
+int main ()
+{
+
+  printf("           +--------------------------------------------------------------+\n           |            Computer Science and Engineering                  |\n           |       CSCE 2110 - Foundation of Data Structures              |\n           |                                                              |\n           |                      Major Assignmment                       |\n           |                                                              |\n           |                   Section 211 Group Members:                 |\n           |                      Brandon Tsuchiya                        |\n           |                       Eric Tsuchiya                           |\n           |               Sahardeed AhmedSahardeed Ahmed                 |\n           |                       Nicolas Rotunno                        |\n           +--------------------------------------------------------------+\n\n\n");
+
+  int selection;
+  printf("\n Welcome to the menu \n ");
+  //
+
+do
+{//start of do
+  printf("\nEnter the menu option for the operation to perform:");
+
+  printf("\n(1) POWER OF 2");
+  printf("\n(2) REVERSE BITS");
+  printf("\n(3) REPLACE BIT POSITION FROM MASK");
+  printf("\n(4) PALINDROME");
+  printf("\n(5) EXIT");
+  printf("\n--> ");
+  scanf("%d", &selection);
+
+  switch (selection)
+   {//start of switch
+     case 1:
+       {
+         //Power
+         break;
+       }
+     case 2:
+       {
+         //Reverse
+         break;
+       }
+     case 3:
+       {
+         //Replace
+         break;
+       }
+     case 4:
+       {
+         //Palindrome
+         break;
+       }
+     case 5:
+       {
+	 printf("Program terminating. Goodbye.\n");
+         //exit(0);
+         break;
+       }
+    }//end of switch
+  }while(selection != 5);//end of do
+
+  return 0;
+}
